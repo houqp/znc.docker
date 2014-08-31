91706c49bb82915b7ca0f3b680caf8cfc4437040
Qingping Hou <qhou@linkedin.com>
2014-08-31 02:35:49 -0700

    fix file permissions

--- znc-image/znc.sh
+++ znc-image/znc.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-exec /sbin/setuser znc-admin znc -f -d /home/znc-admin/.znc >> /var/log/znc.log 2>&1
+chown -R znc-admin:znc-admin /home/znc-admin
+if [ ! -f /home/znc-admin/.znc/znc.pem ]; then
+	# create pem file for ssl
+	exec /sbin/setuser znc-admin znc -p
+fi
+exec /sbin/setuser znc-admin znc -f -d /home/znc-admin/.znc # >> /var/log/znc.log 2>&1
