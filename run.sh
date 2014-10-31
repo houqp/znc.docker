b2c8cbc7d786c6082c30aaacdeb4571e1dc92aa3
Qingping Hou <qihou@linkedin.com>
2014-10-31 12:45:26 -0700

    detect data container in run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,12 @@
 HOSTPORT=5001
 CONTAINERPORT=5001
 
-echo "run znc data container..."
-docker run -d --name znc_data houqp/znc-data
-echo "restoring znc config..."
-bash ./restore.sh
+if [ ! -n "`docker ps -a | grep znc_data`" ]; then
+	docker run -d --name znc_data houqp/znc-data
+	echo "restoring znc config..."
+	bash ./restore.sh
+fi
+
 echo "run znc container..."
 docker run -d -p 0.0.0.0:$HOSTPORT:$CONTAINERPORT \
     --volumes-from znc_data \
